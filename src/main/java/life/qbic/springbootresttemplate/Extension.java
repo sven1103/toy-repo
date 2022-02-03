9ddb53b9437eee0c89a4cdb6b3fd505a9be6dbe8
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2022-02-03 14:17:13 +0100

    BREAKING CHANGE: Its out!

--- src/main/java/life/qbic/springbootresttemplate/Extension.java
+++ src/main/java/life/qbic/springbootresttemplate/Extension.java
@@ -5,6 +5,8 @@ public class Extension {
   public static void main(String[] args) {
     System.out.println("Hi World!");
     System.out.println("Welcome");
+
+    System.out.println("----");
   }
 
 }
